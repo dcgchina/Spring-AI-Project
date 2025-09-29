fc3950308bdb7e1777c7d4b7c2ba06019032e2ba
dcg <2891830259@qq.com>
2025-09-30 00:02:40 +0800

    基于远程向量数据库PGvector的文档持久化和输出

---
+++ src/test/java/com/dg/springaidemo/rag/PgVectorStoreConfigTest.java
@@ -0,0 +1,34 @@
+package com.dg.springaidemo.rag;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class PgVectorStoreConfigTest {
+
+    @Resource
+    private VectorStore pgVectorStoreConfig;
+
+    @Test
+    void pgVectorVectorStore() {
+        List<Document> documents = List.of(
+                new Document("在恋爱过程中怎样保持亲密度？拥抱啊、亲吻啊", Map.of("meta1", "meta1")),
+                new Document("晨光的恋爱手册"),
+                new Document("晨光这个小伙子比较帅气", Map.of("meta2", "meta2")));
+        // 添加文档
+        pgVectorStoreConfig.add(documents);
+        // 相似度查询
+        List<Document> results = pgVectorStoreConfig.similaritySearch(SearchRequest.builder().query("怎么在恋爱过程中保持亲密度啊").topK(3).build());
+        Assertions.assertNotNull(results);
+    }
+}
+
